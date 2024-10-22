d3146d6cb6b02b08c423c52aaa9f3fa38a7308f1
Andrew <Andrew.jayaraju@gmal.com>
2024-10-22 17:33:01 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,12 +9,13 @@ public class Main {
         }
     }
     public static class B extends A{
+        @Override
         public String meth(){
             return "Method is overridden in Extended class B";
         }
     }
     public static void main(String[] args) {
-        B obj = new B();
+        A obj = new B();
         obj.meth();
 
         B obj1 = new B();
